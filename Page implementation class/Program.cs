aea3d48e70c42da5792001bc2dce10444448fa1d
trofim110 <trofim.chuchuk@mail.ru>
2022-07-15 15:49:44 +0700

    p

--- Page implementation class/Program.cs
+++ Page implementation class/Program.cs
@@ -1,9 +1,21 @@
-﻿class PageableCollection
+﻿class PageableCollection<T>
 {
-    private List<string> _page = new List<string>();
-    private int page;
-    private int index;
+    private List<T> _page = new List<T>();
+    private int pageNumber=1;
+    private int index=0;
 
+
+
+    public int PageNumber
+    {
+        get{return  pageNumber;}
+        set
+        {
+            if (value > 0 && value < _page.Count){  pageNumber = value;}
+            else{  Console.WriteLine("Вы вышли за допустимые пределы ");;}
+
+        }
+    }
     public int Index
     {
         get{ return index;}
@@ -11,40 +23,47 @@
         set
         {
             if (value >= 0 && value < _page.Count){ index = value;}
-            else{Console.WriteLine("Ошибка вышли за допустимые пределы индекса");}
+            else{ Console.WriteLine("Ошибка вышли за допустимые пределы индекса");}
         }
     }
 
-    public int Page
-    {
-        get{return page;}
-        set
-        {
-            if (value > 0 && value < _page.Count){ page = value;}
-            else{ Console.WriteLine("Ошибка вышли за допустимые пределы страници");}
 
-        }
-    }
-
-    public PageableCollection(List<string> Page)=> _page = Page; //Конструктор
+    public PageableCollection(List<T> Page)=> _page = Page; //Конструктор
 
 
 
     public int PageCount() {  return _page.Count;} // количество страниц в колекции
-    public int CurrentPageNumber() { return Page-1;}  // Текущий индекс
+    public int CurrentPageNumber() { return PageNumber-1;}  // Текущий индекс
+
+    public int CurrentPage() { return PageNumber;} //  текущая страница
+
 
 
 
     public void TurnPages(int pageCount,string direction) //переворачивает" страницы на pageCount количества в направлении Direction
     {
-        if (direction == "+"){ Page += pageCount;}
-        else if(direction=="-"){ Page-=pageCount;}
+        if (direction == "+"){ PageNumber += pageCount;}
+        else if(direction=="-"){ PageNumber-=pageCount;}
         else{Console.WriteLine("Введите либо + если хотите вперед - если хотите назад");}
     }
 
-    public void TurnPageForward()=> Page++;  //переварачивает" на следующую страницу
-    public void TurnPageBackward()=> Page--; //переворачивает" страницу назад
-    public void OpenPage(int pageNumber)=> Console.WriteLine(_page[Page]);  //устанавливает ("открывает") текущую страницу на pageNumber
+    public void TurnPageForward()=>PageNumber++;  //переварачивает" на следующую страницу
+    public void TurnPageBackward()=> PageNumber--; //переворачивает" страницу назад
+
+    public void OpenPage(int PageNumber) //устанавливает ("открывает") текущую страницу на pageNumber
+    {
+        try
+        {
+            this.PageNumber = PageNumber;
+            Console.WriteLine((_page[PageNumber - 1]));
+        }
+        catch
+        {
+           Console.WriteLine("Вы вышли за допустимые пределы страниц  ");
+        }
+
+    }// Есть исключение
+
 
 
 }
@@ -52,15 +71,22 @@ class project
 {
     static void Main()
     {
-        List<string> page = new List<string>(){"111","2222","3333","4444"};
-        PageableCollection aaa = new PageableCollection(page);
-
-        Console.WriteLine(aaa.Page);
-        Console.WriteLine(aaa.Page);
-        Console.WriteLine(aaa.Page);
-        Console.WriteLine(aaa.Page);
-        Console.WriteLine(aaa.Page);
 
 
+            var page = new List<string>(){"343","2222","3333","4444"};
+            var aaa = new PageableCollection<string>(page);
+            Console.WriteLine(aaa.CurrentPage());
+            aaa.TurnPageForward();
+            Console.WriteLine(aaa.CurrentPage());
+            aaa.TurnPageForward();
+            Console.WriteLine(aaa.CurrentPage());
+            aaa.TurnPageForward();
+            Console.WriteLine(aaa.CurrentPage());
+            aaa.TurnPageForward();
+            Console.WriteLine(aaa.CurrentPage());
+            aaa.OpenPage(5);
+            Console.WriteLine(aaa.CurrentPage());
+            aaa.TurnPages(2, "+");
     }
 }
+
--- Page implementation classTest/UnitTest1.cs
+++ Page implementation classTest/UnitTest1.cs
@@ -2,10 +2,17 @@ using Xunit;
 
 namespace Page_implementation_classTest;
 
+class book
+{
+
+}
+
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        
+
     }
 }
